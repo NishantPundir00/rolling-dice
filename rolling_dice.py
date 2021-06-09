af58b16e1bdd1c2f3517dfc2520bf4dfa5b5477a
Nishant Pundir <pundirnishant0@gmail.com>
2021-06-09 23:35:06 +0530

    Add files via upload

---
+++ rolling_dice.py
@@ -0,0 +1,22 @@
+import dices #import dices
+import random #import random libary
+
+num = random.randrange(1,7) #random number 1-6
+
+if num == 1:
+    dices.dice_one()
+
+if num == 2:
+    dices.dice_two()
+
+if num == 3:
+    dices.dice_three()
+
+if num == 4:
+    dices.dice_four()
+
+if num == 5:
+    dices.dice_five()
+
+if num == 6:
+    dices.dice_six()
